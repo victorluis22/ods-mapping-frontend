5533be0c61a4f846fe286379ecdb6a9fe993d38e
Victor Luis Teixeira Reis <victorluis2002@gmail.com>
2023-12-13 14:17:00 -0300

    resolvendo problema com .env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const env = loadEnv(mode, process.cwd(), '');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.API_URL': JSON.stringify(env.API_URL)
+  },
   plugins: [react()]
 })
