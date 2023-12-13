5f29f7dd54c317b0bf5177c717cacac3a6d4bc6f
Victor Luis Teixeira Reis <victorluis2002@gmail.com>
2023-12-13 14:08:17 -0300

    configurando api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://hack2030-backend.onrender.com/'
+    baseURL: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://localhost:5000" : process.env.API_URL
     // baseURL: 'http://localhost:5000'
 })
 
