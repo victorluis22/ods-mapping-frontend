20beeaee79aa34ad95579d3b8767af6c5e9665dc
Kauan Peçanha <kauanpecanha1@gmail.com>
2023-12-14 20:58:31 -0300

    footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,30 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import serraLogo from '../../assets/serraLogo.svg'
-
+import odsMapping from "../../assets/odsMapping.svg"
 import './style.css'
 
 const Footer = () => {
     return(
         <div className="Footer">
-            <div className="Pall">
-                <div className="FooterP1">
+            {/* <div className="Pall"> */}
+                {/* <div className="FooterP1">
                     <p>© Serra HackClub 2023</p>
-                </div>   
-                <div>
+                </div>    */}
+                {/* <div>
                     <a target='_blank' href="https://www.instagram.com/serrahackclub">
                         <img src={serraLogo} alt="" />
                     </a>
-                </div>
-                <div className="FooterP2">
+                </div> */}
+                {/* <div className="FooterP2">
                     <p>Todos os direitos reservados</p>
-                </div> 
-            </div>
-            <p className="terms-privacy">
+                </div>  */}
+            {/* </div> */}
+            {/* <p className="terms-privacy">
                 <Link to="/termos">Termos de serviço</Link> e <Link to="/privacidade">política de privacidade</Link>
-            </p>
+            </p> */}
+
             
-               
+            <img src={serraLogo} className="SoftwareLogo" alt="" />
+            <img src={odsMapping} className="SoftwareLogo" alt="" />
             
         </div>
     )
